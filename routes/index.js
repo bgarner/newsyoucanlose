1225651ebe72666525346eb71928ea16a472f8d5
Brent Garner <bgarner@gmail.com>
2017-01-26 14:28:37 -0700

    wip

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,18 @@ router.get('/', function(req, res, next) {
 
 
 
+// <script src="https://www.gstatic.com/firebasejs/3.6.7/firebase.js"></script>
+// <script>
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyDFd5RSehK890o55wXyf2UwGxBAu-hSoiI",
+//     authDomain: "newsyoucanlose.firebaseapp.com",
+//     databaseURL: "https://newsyoucanlose.firebaseio.com",
+//     storageBucket: "newsyoucanlose.appspot.com",
+//     messagingSenderId: "338410298978"
+//   };
+//   firebase.initializeApp(config);
+// </script>
+
+
 module.exports = router;
